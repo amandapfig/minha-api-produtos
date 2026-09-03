a6259d53232a2baf86a1a8e0691593e4531929a3
amandapfig <amandapfigueiredo55@gmail.com>
2026-09-03 20:39:50 -0300

    Add alteração no server.js

--- server.js
+++ server.js
@@ -111,8 +111,8 @@ app.put('/produtos/:id', (req, res) => {
     }
 })
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`Servidor ouvindo em http://localhost:${port}`)
+    console.log(`Servidor ouvindo na porta ${port}`)
 })
